488eed5bbfa9d764179bd1a8c42ee96844ea6caf
Yeison Monsalve <168552590+YeiMonsalve@users.noreply.github.com>
2024-08-06 17:29:47 -0500

    Se agregó un if para que al dar una respuesta y no haya datos mande un mensaje sobre que no existe en la base de datos

--- src/controllers/getUsuarioById.controller.js
+++ src/controllers/getUsuarioById.controller.js
@@ -2,12 +2,19 @@ const { connectDb } = require('../database');
 
 async function getUsuarioById(req, res) {
     const client = await connectDb();
-    const {id} = req.body
+    const { id } = req.body
     const respuesta = await client.query('SELECT * FROM usuarios WHERE id = $1', [id]);
-    res.send({
-        message: 'ok',
-        data: respuesta.rows
-    });
+    if (respuesta.rows.length > 0) {
+        res.send({
+            'message': 'ok',
+            'data': respuesta.rows
+        })
+    } else {
+        res.send({
+            'message': 'ok',
+            'data': `no se encontro datos para el id ${id}`
+        })
+    }
 }
 
 module.exports = getUsuarioById;
